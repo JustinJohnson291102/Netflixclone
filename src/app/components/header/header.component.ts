c60eccf0e46060ca3f61fd47dcbd766b5665686f
ShinuJohnson1972 <shinu.p.johnson@gmail.com>
2025-09-18 17:20:22 +0530

    Fix unterminated img tag in header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -62,7 +62,6 @@ import { Subject, from, of } from 'rxjs';
             </div>
 
             <div class="profile">
-              <img 
             <div class="profile-avatar">
               <svg width="32" height="32" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z"/>
